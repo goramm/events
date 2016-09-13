16731dc17676032298758feed0109ecf324a1323
Goran Milovanović <goramm@gmail.com>
2016-09-13 02:11:41 +0200

    add seed_dump gem and export data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.create!([
+  {name: "Bug Smash", location: "Denver, CO", price: "0.0", starts_at: "2016-12-06 19:23:00", description: "A fun event of bug smashing!", image_file_name: "bugsmash.png", capacity: 10},
+  {name: "Hackaton", location: "Austin, TX", price: "15.0", starts_at: "2016-09-07 19:25:00", description: "Hunker down at the Hackathon!", image_file_name: "hackathon.png", capacity: 50},
+  {name: "Kata Camp", location: "Dallas, TX", price: "75.0", starts_at: "2016-09-23 00:00:00", description: "Practice your craft kata style!", image_file_name: "katacamp.jpg", capacity: 5},
+  {name: "Past event", location: "Zagreb, HR", price: "0.0", starts_at: "2016-08-28 22:13:00", description: "", image_file_name: "", capacity: 1}
+])
