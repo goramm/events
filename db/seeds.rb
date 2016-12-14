af490c564b658dfd5109dfb14ca5cda6535eff98
Goran Milovanovic <goramm@gmail.com>
2016-12-14 22:01:23 +0100

    long description for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Event.create!([
-  {name: "Bug Smash", location: "Denver, CO", price: "0.0", starts_at: "2016-12-06 19:23:00", description: "A fun event of bug smashing!", image_file_name: "bugsmash.png", capacity: 10},
-  {name: "Hackaton", location: "Austin, TX", price: "15.0", starts_at: "2016-09-07 19:25:00", description: "Hunker down at the Hackathon!", image_file_name: "hackathon.png", capacity: 50},
-  {name: "Kata Camp", location: "Dallas, TX", price: "75.0", starts_at: "2016-09-23 00:00:00", description: "Practice your craft kata style!", image_file_name: "katacamp.jpg", capacity: 5},
-  {name: "Past event", location: "Zagreb, HR", price: "0.0", starts_at: "2016-08-28 22:13:00", description: "", image_file_name: "", capacity: 1}
+  {name: "Bug Smash", location: "Denver, CO", price: "0.0", starts_at: "2016-12-06 19:23:00", description: "A fun event of bug smashing! A fun event of bug smashing! A fun event of bug smashing! ", image_file_name: "bugsmash.png", capacity: 10},
+  {name: "Hackaton", location: "Austin, TX", price: "15.0", starts_at: "2016-09-07 19:25:00", description: "Hunker down at the Hackathon! Hunker down at the Hackathon! Hunker down at the Hackathon!", image_file_name: "hackathon.png", capacity: 50},
+  {name: "Kata Camp", location: "Dallas, TX", price: "75.0", starts_at: "2016-09-23 00:00:00", description: "Practice your craft kata style! Practice your craft kata style! Practice your craft kata style!", image_file_name: "katacamp.jpg", capacity: 5},
+  {name: "Past event", location: "Zagreb, HR", price: "0.0", starts_at: "2016-08-28 22:13:00", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", image_file_name: "", capacity: 1}
 ])
